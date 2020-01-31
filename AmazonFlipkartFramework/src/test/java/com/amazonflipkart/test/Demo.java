36a3e2c987591898ae57ffb14831a462cf0d77a3
shiva252010 <srishasrinika@gmail.com>
2020-01-31 14:04:00 +0530

    My second commit

---
+++ AmazonFlipkartFramework/src/test/java/com/amazonflipkart/test/Demo.java
@@ -0,0 +1,13 @@
+package com.amazonflipkart.test;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void jenkinsTry()
+	{
+		System.out.println(System.getProperty("url"));
+		System.out.println(System.getProperty("browser"));
+	}
+
+}
